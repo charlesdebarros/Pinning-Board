9566870046e73a1cf1a472e365d42a97addead55
Charles De Barros <charlesdebarros@yahoo.co.uk>
2018-03-28 14:49:57 +0100

    Updating loofah gem to sort a security issue raise by GitHub.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create({email: 'user@test.com', password: 'Password1$', password_confirmation: 'Password1$'})
+pin = Pin.create({
+  user_id: 1,
+  title: 'Imperatriz Leopoldinense',
+  description: 'Tradicional escola do carnaval carioca. Fundada em 03 de Maio de 1959, localizada na Zona da Leopoldina, suburbio carioca.',
+  image_file_name: 'blogger-image--954831936.jpg',
+  image_content_type: 'image/jpeg'
+})
