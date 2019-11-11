67fe27bf7fba8a5a2f6a32bdce3b2efe056abfd2
Charles De Barros <charlesdebarros@yahoo.co.uk>
2019-11-11 12:07:33 +0000

    Updating "db/seeds.rb" file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create({email: 'user@test.com', password: 'Password1$', password_confirmation: 'Password1$'})
-pin = Pin.create({
+User.create(
+  email: 'user@test.com', password: 'Password1$', password_confirmation: 'Password1$'
+)
+
+Pin.create(
   user_id: 1,
   title: 'Imperatriz Leopoldinense',
   description: 'Tradicional escola do carnaval carioca. Fundada em 03 de Maio de 1959, localizada na Zona da Leopoldina, suburbio carioca.',
   image_file_name: 'blogger-image--954831936.jpg',
   image_content_type: 'image/jpeg'
-})
+)
+Pin.create(
+  user_id: 1,
+  title: 'Porto da Pedra',
+  description: 'Escola vermelha e branco de São Gonçalo, na região metropolitana do Rio de Janeiro. Fundada em 08 de Março de 1978. Tem o tigre como seu símbolo.',
+  image_file_name: 'porto_da_pedra.jpg',
+  image_content_type: 'image/jpeg'
+)
+Pin.create(
+  user_id: 1,
+  title: 'Unidos do Viradouro',
+  description: 'Tradicional escola de Niterói, na região metropolitana do Rio de Janeiro. Fundada em 24 de Junho de 1946. Suas cores são o vermelho e o branco.',
+  image_file_name: 'viradouro.jpg',
+  image_content_type: 'image/jpeg'
+)
